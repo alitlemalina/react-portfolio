9aaaf1f2b282468099374042abed1326df1e9fc9
alitlemalina <57286752+alitlemalina@users.noreply.github.com>
2021-06-19 17:49:23 +0700

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -34,7 +34,7 @@ class NavBar extends React.Component {
       
         <div className="container" >
   
-          <a className="navbar-brand" href="home"><img className="logo" src={logo} alt="logo..." /></a>
+          <a className="navbar-brand" href="#"><img className="logo" src={logo} alt="logo..." /></a>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <FontAwesomeIcon icon={faBars} style={{ color: "#fff" }} />
           </button>
@@ -42,7 +42,7 @@ class NavBar extends React.Component {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav ml-auto">
               <li className="nav-item active">
-                <a className="nav-link" href="home">Home</a>
+                <a className="nav-link" href="#">Home</a>
               </li>
               <li className="nav-item">
                 <a className="nav-link" href="#AboutMe">About me</a>
